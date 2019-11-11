246a2c42163a3eab85d9baee8b5fe953c346ed66
anitta <anitta@appirits.com>
2019-11-11 22:50:46 +0900

    mailsetting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,22 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "tasker_#{Rails.env}"
 
-  config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'https://task-share-ak.herokuapp.com'}
+  #config.action_mailer.perform_caching = false
+  #config.action_mailer.default_url_options = { host: 'https://task-share-ak.herokuapp.com'}
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://task-share-ak.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -79,13 +93,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.raise_delivery_errors = false
-   config.action_mailer.delivery_method = :smtp
-   config.action_mailer.smtp_settings = {
-   address: "navcrk5@gmail.com",
-   port: 2525, # 8025, 587 and 25 can also be used.
-   domain: "gmail.com",
-  }
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
